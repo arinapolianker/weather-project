09d6256bb266a35fddd822e230a3042825ba0f67
arinapolianker <arinapolianker@gmail.com>
2021-12-13 12:04:41 -0500

    Adding the html, css and js files from sandbox

---
+++ weather.js
@@ -0,0 +1,104 @@
+// current day+time //
+let now = new Date();
+let hour = now.getHours();
+let min = now.getMinutes();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+if (min < 10) {
+  min = `0${min}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Teausday",
+  "Wednsday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let currentTime = document.querySelector("h5");
+currentTime.innerHTML = `${day} ${hour}:${min}`;
+// Open page with current info //
+let apiKey = "089de42863cffcedc265abdd75619b42";
+navigator.geolocation.getCurrentPosition(getCurrentLocation);
+
+function getCurrentLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemp);
+}
+// search city //
+let form = document.querySelector("#form");
+function currentCity(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#search");
+  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&units=metric&appid=${apiKey}`;
+  let h3 = document.querySelector("h3");
+  h3.innerHTML = `${searchCity.value}`;
+  axios.get(apiUrlCity).then(showTemp);
+}
+form.addEventListener("submit", currentCity);
+// temp functions //
+function showTemp(response) {
+  let temp = Math.round(response.data.main.temp);
+  let deg = document.querySelector("#deg");
+  deg.innerHTML = `${temp}°C`;
+  console.log(temp);
+  let feels = document.querySelector("#f");
+  let feelslike = Math.round(response.data.main.feels_like);
+  feels.innerHTML = `${feelslike}°C`;
+  function degC(event) {
+    event.preventDefault();
+    let butC = temp;
+    deg.innerHTML = `${butC}°C`;
+  }
+  function degF(event) {
+    event.preventDefault();
+    let butF = Math.round((temp * 9) / 5 + 32);
+    deg.innerHTML = `${butF}°F`;
+    let feels = document.querySelector("#f");
+    let feelsDegF = Math.round((feelslike * 9) / 5 + 32);
+    feels.innerHTML = `${feelsDegF}°F`;
+  }
+  let butc = document.querySelector(".butC");
+  let butf = document.querySelector(".butF");
+  butc.addEventListener("click", degC);
+  butf.addEventListener("click", degF);
+
+  let currentCity = response.data.name;
+  console.log(currentCity);
+  let h3 = document.querySelector("h3");
+  h3.innerHTML = currentCity;
+
+  let wind = Math.round(response.data.wind.speed);
+  let windspeed = document.querySelector("#w");
+  windspeed.innerHTML = `${wind} km/h`;
+  let humidity = response.data.main.humidity;
+  let hum = document.querySelector("#h");
+  hum.innerHTML = `${humidity}%`;
+
+  let weatherKind = response.data.weather[0].main;
+  let h4 = document.querySelector("h4");
+  h4.innerHTML = weatherKind;
+}
+
+// current location + temp //
+
+function currentPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemp);
+}
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+
+let place = document.querySelector(".current");
+place.addEventListener("click", getCurrentPosition);
